4a8e86dad0f1c1b1f5b74d8210b2773b50639eff
Arnab <arnabdnri@gmail.com>
2022-06-17 22:11:46 +0800

    Add event listeners for buttons

--- app.js
+++ app.js
@@ -22,3 +22,21 @@ function operate (a, b, op) {
         case '/': return a / b;
     }
 }
+
+const button = document.getElementsByClassName('button');
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('mouseenter', () => { 
+        button[i].style.backgroundColor = 'gray';
+})
+    button[i].addEventListener('mouseleave', () => {
+        button[i].style.backgroundColor = 'lightgray';
+    })
+}
+
+const clear = document.getElementById('clear');
+clear.addEventListener('mouseenter', () => {clear.style.backgroundColor = 'red';})
+clear.addEventListener('mouseleave', () => {clear.style.backgroundColor = 'pink';})
+
+const del = document.getElementById('delete');
+del.addEventListener('mouseenter', () => {del.style.backgroundColor = 'blue';})
+del.addEventListener('mouseleave', () => {del.style.backgroundColor = 'lightblue';})
